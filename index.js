911530d8b7e9a87eb51c91b08727997c9db1722d
deputatovn <deputatovn@gmail.com>
2022-02-23 15:18:33 +0300

    Fix function

--- index.js
+++ index.js
@@ -1,54 +1,57 @@
-'use strict'
+"use strict";
 
-function min_0 (n1, n2, n3, n4) {
-  return [n1, n2, n3, n4].sort((a,b) => a-b)[0];
+function min_0(n1, n2, n3, n4) {
+  return [n1, n2, n3, n4].sort((a, b) => a - b)[0];
 }
 
-function min_1 () {
-  return [...arguments].sort((a, b) => a-b)[0];
+function min_1() {
+  return [...arguments].sort((a, b) => a - b)[0];
 }
 
-function min_2 (...args) {
-  return args.sort((a, b) => a-b)[0];
+function min_2(...args) {
+  return args.sort((a, b) => a - b)[0];
 }
 
 const min_3 = (n1, n2, n3, n4) => {
   let min = n1;
 
   if (n2 < min) {
-    min = n2
+    min = n2;
   }
 
   if (n3 < min) {
-    min = n3
+    min = n3;
   }
 
   if (n4 < min) {
-    min = n4
+    min = n4;
   }
 
   return min;
-}
+};
 
-
-function getNumber () {
+function getNumber() {
   let num;
 
   do {
-    num = prompt("Введите валидное число", 0)
+    num = prompt("Введите валидное число", 0);
   } while (!isFinite(num));
 
-  if (num === null || num === '') return null;
+  if (num === null || num === "") return null;
 
   return +num;
 }
 
-const validator = num => num > 18
+const validator = (num) => num > 18;
 
-function getNumberWithValidator (validator) {
+function getNumberWithValidator(validator) {
   let num;
 
   do {
-    num = +prompt("Введите валидное число", 0)
-  } while (validator(num))
+    num = +prompt("Введите валидное число", 0);
+  } while (validator(num));
+
+  if (num === null || num === "") return null;
+
+  return +num;
 }
